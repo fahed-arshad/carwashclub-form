f126fcb0c4c88b04b292bbeae32531383eba22ec
fahedarshad01 <fahed.arshad@ovo.com>
2024-10-24 16:16:08 +0100

    test iphone

--- car-wash-club-form/src/app/theme/theme.ts
+++ car-wash-club-form/src/app/theme/theme.ts
@@ -89,6 +89,10 @@ export const theme = createTheme({
       styleOverrides: {
         root: {
           backgroundColor: "#212121",
+          "&:-webkit-autofill": {
+            WebkitBoxShadow: "0 0 0px 1000px #212121 inset", // Override autofill background color
+            WebkitTextFillColor: "#FFFFFF", // Change text color to match your theme
+          },
         },
         focused: {
           borderColor: "#FFFFFF!important",
