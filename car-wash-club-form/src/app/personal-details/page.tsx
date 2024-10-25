41c6a32beb91248ffcad8fdb7b46dff81bd79d91
fahedarshad01 <fahed.arshad@ovo.com>
2024-10-25 11:36:18 +0100

    attempt with polyfill

--- car-wash-club-form/src/app/personal-details/page.tsx
+++ car-wash-club-form/src/app/personal-details/page.tsx
@@ -21,7 +21,7 @@ import { useRouter } from "next/navigation";
 import Grid from "@mui/material/Grid2";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { steps } from "../component/steps";
-import { scrollIntoView } from "seamless-scroll-polyfill";
+import { polyfill } from "seamless-scroll-polyfill";
 
 export interface PersonalDetailsData {
   firstName: string;
@@ -39,9 +39,10 @@ export default function OnboardingForm() {
 
   useEffect(() => {
     // Scroll to the form element when the page loads
+    polyfill();
 
     if (formRef.current) {
-      scrollIntoView(formRef.current, {
+      formRef.current.scrollIntoView({
         behavior: "smooth",
         block: "center",
         inline: "center",
