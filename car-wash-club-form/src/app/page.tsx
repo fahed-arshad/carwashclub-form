d5cff67a84fb3a69abc9eabae1af619e396ef877
fahedarshad01 <fahed.arshad@ovo.com>
2024-10-30 00:19:11 +0000

    revalidate on refresh

--- car-wash-club-form/src/app/page.tsx
+++ car-wash-club-form/src/app/page.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import Home from "./form";
 
+export const revalidate = 0;
+
 export interface Membership {
   id: number;
   name: string;
@@ -10,7 +12,12 @@ export interface Membership {
 
 export default async function HomeSSR() {
   const { data } = await axios.get<Membership[]>(
-    `${process.env.NEXT_PUBLIC_BACKEND_API}/memberships`
+    `${process.env.NEXT_PUBLIC_BACKEND_API}/memberships`,
+    {
+      headers: {
+        "Cache-Control": "no-store",
+      },
+    }
   );
   return <Home memberships={data} />;
 }
